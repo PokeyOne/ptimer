22781408d2a41f9eedbebbf840c3a94b45c40e56
Mateo Carreras <mateo.carreras@gmail.com>
2021-09-25 00:41:00 -0700

    program takes an input and outputs

--- rust_version/ptimer/src/main.rs
+++ rust_version/ptimer/src/main.rs
@@ -1,3 +1,14 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Please standby while we process your request.");
+    println!("An agent will be with you shortly.");
+
+    let mut blah = String::new();
+
+    io::stdin()
+        .read_line(&mut blah)
+        .expect("Failed to read line");
+
+    println!("Blah was {}", blah);
 }
